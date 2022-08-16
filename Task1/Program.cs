a475db77f4679ec7a7d8a739b6f6a2b679abe923
Alexey <pwz-and_10@mail.ru>
2022-08-16 22:08:48 +0600

    Create Task1 on Seminar004

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿void PowerDeg()
+{
+int NaturalDegree(int A, int B)
+{
+    int result = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        result = result * A;
+        }
+        return result;
+        }
+
+  Console.Write("Число A: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Число B: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+
+  int exp = NaturalDegree(A, B);
+  Console.WriteLine("Результат: " + exp);
+}
+PowerDeg();
