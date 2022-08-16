bb44e831b1e391d23b5a3949c5972d56bf631d53
Alexey <pwz-and_10@mail.ru>
2022-08-16 22:22:26 +0600

    Create Task2 on Seminar004

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿void SumNumbers()
+{
+    Console.Write("Число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int Summ(int a)
+    {
+        int count = Convert.ToString(a).Length;
+        int advance = 0;
+        int result = 0;
+        for (int i = 0; i < count; i++)
+        {
+            advance = a - a % 10;
+            result = result + (a - advance);
+            a = a / 10;
+            }
+            return result;
+            }
+            int summ = Summ(a);
+            Console.WriteLine("Результат: " + summ);
+}
+SumNumbers();
