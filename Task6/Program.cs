1f44b47ddf0aa5ccb55724c9e6ef5163fc259bc3
Alexey <pwz-and_10@mail.ru>
2022-08-21 18:32:02 +0600

    create task6

---
+++ Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int input;
+        do
+        {
+            Console.Write("Введите число: ");
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("{0}", Convert.ToString(input, 2));
+            }
+            Console.WriteLine();
+        }
+        while (true);
+    }
+}
